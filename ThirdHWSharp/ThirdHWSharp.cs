bc687cb076d1f70715ab8e9b365b5ddbae0aef88
Julia <krasotylicka@mail.ru>
2022-10-14 00:30:50 +0300

    Homework finished

---
+++ ThirdHWSharp/ThirdHWSharp.cs
@@ -0,0 +1,67 @@
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+    // Павел, метод работает, я проверила на цифрах, но в 14-17 есть ошибка и на все числа пишет - НЕТ! Не могу найти. :(
+
+/*
+void CheckingPalindromArray(string number)
+{
+    if(number[0] == number[4] && number[1] == number[3])
+        Console.Write("Yes your number is a polindrom!");
+    else
+        Console.Write("Your number isn't a polindrom!");
+}
+   //CheckingPalindromArray("12321");
+
+Console.Write("Input five-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+CheckingPalindromArray("number");
+int palindrom = Convert.ToInt32(number);
+
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+/*
+double FindDistance (double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    //double dis = 0;
+    double A = Math.Pow(x2 - x1, 2);
+    double B = Math.Pow(y2 - y1, 2);
+    double C = Math.Pow(z1 - z2, 2);
+    double D = A * A + B * B + C * C;
+    double G = Math.Sqrt(D);
+    return G;
+}
+
+Console.Write("Input x1-coordinat: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input y1-coordinat: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input z1-coordinat: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input x2-coordinat: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input y2-coordinat: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input z2-coordinat: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double length = FindDistance(x1, y1, z1, x2, y2, z2);
+Console.Write($"Еhe length of the segment is equal to {length}");
+*/
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+/*
+void FindСubeNumbers(int num)
+{
+    int count = 1;
+    while(count <= num)
+    {
+        double n = Math.Pow(count, 3);
+        Console.Write(n + " ");
+        count++;
+
+    }
+}
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+FindСubeNumbers(number);
+*/

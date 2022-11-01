b403278e31715b0c27a52af51dd5610028cfde85
Julia <krasotylicka@mail.ru>
2022-11-01 23:06:25 +0300

    Second task added

--- SixthHWSharp/SixthHWSharp.cs
+++ SixthHWSharp/SixthHWSharp.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-
+/*
 int[] CreateRandomArray()
 {
     Console.Write("Input number of elements: ");
@@ -41,3 +41,21 @@ int[] myArray = CreateRandomArray();
 ShowArray(myArray);
 int countNum = CountNumbers(myArray);
 Console.WriteLine($"The count of numbers: {countNum}");
+*/
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Write("Input a value of b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input a value of k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input a value of b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input a value of k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Coordinates of the intersection point of the lines:({x};{y}).");
+
